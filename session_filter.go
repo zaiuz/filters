f68922839068e6276fe56e528fdef16fb5289fc9
Chakrit Wichian <service@chakrit.net>
2014-02-11 02:05:35 +0700

    Session module reimplemented as Filter.

--- session_filter.go
+++ session_filter.go
@@ -1,50 +1,47 @@
-package sessions
+package filters
 
 import "github.com/gorilla/sessions"
-import "errors"
 import z "github.com/zaiuz/zaiuz"
 
-const ContextKey = "zaius.SessionModule"
+const SessionFilterPrefix = "z.session"
 
-type SessionModule struct {
+type sessionFilterData struct {
 	store sessions.Store
 	name  string
 }
 
-var _ z.Module = new(SessionModule)
-
-func CookieSession(name, secret string) *SessionModule {
+func SessionFilter(name, secret string) z.Filter {
 	store := sessions.NewCookieStore([]byte(secret))
-	return &SessionModule{store, name}
-}
+	data := &sessionFilterData{store, name}
 
-func (s *SessionModule) Attach(c *z.Context) error {
-	// TODO: Access sessions lazily
-	session, e := s.store.Get(c.Request, s.name)
-	if e != nil {
-		return e
+	return func(action z.Action) z.Action {
+		return func(c *z.Context) z.Result {
+			c.Set(SessionFilterPrefix+".data", data)
+			result := action(c)
+
+			dirty, ok := c.GetOk(SessionFilterPrefix+".dirty")
+			if ok && dirty.(bool) {
+				session := GetSession(c)
+				e := session.Save(c.Request, c.ResponseWriter)
+				noError(e)
+			}
+
+			return result
+		}
 	}
-
-	c.Set(ContextKey, session)
-	return nil
 }
 
-func Get(c *z.Context) (session *sessions.Session, e error) {
-	result, exists := c.GetOk(ContextKey)
-	if !exists {
-		return nil, errors.New("Get() requires session module in current route.")
-	}
-
-	// TODO: Soft type assert
-	return result.(*sessions.Session), nil
-}
-
-func (s *SessionModule) Detach(c *z.Context) error {
-	result, exists := c.GetOk(ContextKey)
-	if !exists {
+func GetSession(c *z.Context) *sessions.Session {
+	data_, ok := c.GetOk(SessionFilterPrefix+".data")
+	if !ok {
 		return nil
 	}
 
-	session := result.(*sessions.Session)
-	return session.Save(c.Request, c.ResponseWriter)
+	data := data_.(*sessionFilterData)
+	session, e := data.store.Get(c.Request, data.name)
+	noError(e)
+
+	c.Set(SessionFilterPrefix+".dirty", true)
+	return session
 }
+
